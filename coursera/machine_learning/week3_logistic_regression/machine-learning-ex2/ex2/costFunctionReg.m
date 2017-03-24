6dd33d79871cb6f94eb8b64520cba277491b4188
Yasen Marinov <yasen.marinov@experian.com>
2017-03-24 13:56:08 +0200

    ml

--- coursera/machine_learning/week3_logistic_regression/machine-learning-ex2/ex2/costFunctionReg.m
+++ coursera/machine_learning/week3_logistic_regression/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X * theta);
+theta2 = theta;
+theta2(1) = 0;
+J = 1/m * (-y' * log(h) - (1 - y)' * log(1 - h)) + lambda/(2*m) * theta2' * theta2;
 
-
-
+grad = 1/m * (X' * (h - y)) + lambda / m * theta2;
 
 
 % =============================================================
