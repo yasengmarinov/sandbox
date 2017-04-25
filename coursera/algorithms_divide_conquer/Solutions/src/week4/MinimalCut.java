1752e45b311c07072853635e06f4614b3a7377a4
Yasen Marinov <yasen.marinov@experian.com>
2017-04-25 18:18:42 +0300

    alg_week4

--- coursera/algorithms_divide_conquer/Solutions/src/week4/MinimalCut.java
+++ coursera/algorithms_divide_conquer/Solutions/src/week4/MinimalCut.java
@@ -21,8 +21,8 @@ public class MinimalCut {
             e.printStackTrace();
         }
 
-        List<Vertex> verticles = new ArrayList(200);
-        for (int i = 1; i <= 200; i++) {
+        List<Vertex> verticles = new ArrayList<>(input.size());
+        for (int i = 1; i <= input.size(); i++) {
             verticles.add(new Vertex(i));
         }
         List<Edge> edges = new ArrayList<>(1000);
@@ -30,31 +30,114 @@ public class MinimalCut {
         for (int j = 0; j < verticles.size(); j++) {
             String[] numbers = input.get(j).toString().split("\t");
             for (int i = 1; i < numbers.length; i++) {
-                edges.add(new Edge(verticles.get(j), verticles.get(Integer.valueOf(numbers[i]) - 1)));
+                if (Integer.valueOf(numbers[i]) > j + 1)
+                    edges.add(new Edge(verticles.get(j), verticles.get(Integer.valueOf(numbers[i]) - 1)));
             }
         }
-        System.out.println("Done");
+        System.out.println(findMinCut(verticles, edges));
+
+    }
+
+    private static int findMinCut(List<Vertex> vertices, List<Edge> edges) {
+        int minCut = edges.size();
+        for (int i = 0; i < vertices.size(); i++) {
+            int currMin = kargerMinCut(vertices, edges);
+            minCut = (currMin < minCut)? currMin : minCut;
+        }
+
+        return minCut;
+    }
+
+    private static int kargerMinCut(List<Vertex> vertices, List<Edge> edges) {
+
+        List<Vertex> verticesCopy = new ArrayList<>(vertices.size());
+        List<Edge> edgesCopy = new ArrayList<>(edges.size());
+
+        for (Vertex vertex : vertices)
+            verticesCopy.add(vertex.clone());
+        for (Edge edge : edges)
+            edgesCopy.add(edge.clone(verticesCopy));
+
+        int verticlesCount = verticesCopy.size();
+        while (verticlesCount > 2) {
+
+            int randomIndex = (int) Math.floor(Math.random() * edgesCopy.size());
+            // Remove edge
+            Edge removed = edgesCopy.remove(randomIndex);
+            // Merge verticles
+            mergeVerticles(verticesCopy, removed.getVer2().getId(), removed.getVer1().getId());
+
+            // Remove self loops
+            removeSelfLoops(edgesCopy);
+
+            verticlesCount--;
+        }
+
+        return edgesCopy.size();
+    }
+
+    private static void mergeVerticles(List<Vertex> vertices, int id1, int id2) {
+        for (Vertex vertex: vertices) {
+            if (vertex.getId() == id1)
+                vertex.setId(id2);
+        }
+    }
+
+    private static void removeSelfLoops(List<Edge> edgesCopy) {
+        for (int i = edgesCopy.size() - 1; i >= 0; i--) {
+            Edge edge = edgesCopy.get(i);
+            if (edge.getVer1().getId() == edge.getVer2().getId()) {
+                edgesCopy.remove(i);
+            }
+        }
     }
 
     private static class Edge {
-        Vertex ver1, ver2;
+        private Vertex ver1, ver2;
+
         public Edge(Vertex ver1, Vertex ver2) {
             this.ver1 = ver1;
             this.ver2 = ver2;
         }
+
+        public Vertex getVer1() {
+            return ver1;
+        }
+
+        public Vertex getVer2() {
+            return ver2;
+        }
+
         public String toString() {
             return new StringBuilder().append(ver1).
                     append(", ").append(ver2).toString();
         }
+
+        public Edge clone(List<Vertex> vertices) {
+            return new Edge(vertices.get(ver1.getId() - 1), vertices.get(ver2.getId() - 1));
+        }
     }
 
     private static class Vertex {
-        int id;
+        private int id;
         public Vertex(int n) {
             this.id = n;
         }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
         public String toString() {
             return String.valueOf(id);
+        }
+
+        public Vertex clone() {
+            return new Vertex(id);
         }
     }
 
