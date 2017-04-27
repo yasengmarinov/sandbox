cf0eee4d992281958973353812e0e41dade26f5f
Yasen Marinov <yasen.marinov@experian.com>
2017-04-27 19:09:49 +0300

    alg2_week1

---
+++ coursera/algorithms_divide_conquer/Solutions/src/course2/week1/SCCFinder.java
@@ -0,0 +1,96 @@
+package course2.week1;
+
+import java.util.*;
+
+/**
+ * Created by b06514a on 4/27/2017.
+ */
+public class SCCFinder {
+
+    private final List<List<Integer>> graphList;
+    private List<List<Integer>> workingGraph;
+    private final int verticesCount;
+    private boolean[] visited;
+    private int[] t, parents;
+    private int tCounter;
+
+    public SCCFinder(List<List<Integer>> graphList) {
+        this.graphList = graphList;
+        workingGraph = reverseList(graphList);
+        verticesCount = graphList.size();
+
+        visited = new boolean[verticesCount];
+        t = new int[verticesCount];
+        parents = new int[verticesCount];
+        tCounter = verticesCount;
+    }
+
+    private List<List<Integer>> reverseList(List<List<Integer>> graphList) {
+        List<List<Integer>> copy = new ArrayList<>(graphList.size());
+        for (int i = 0; i < graphList.size(); i++) {
+            copy.add(new LinkedList<>());
+        }
+        for (int i = 0; i < graphList.size(); i++) {
+            for (Object vertex : graphList.get(i)) {
+                copy.get(Integer.valueOf(vertex.toString()) - 1).add(i + 1);
+            }
+        }
+        return copy;
+    }
+
+    public int[] find() {
+        for (int i = verticesCount; i > 0; i--) {
+            if (!isVertexVisited(i)) {
+                exploreVertex(i, 0);
+            }
+        }
+        workingGraph = graphList;
+        int parentCount = 0;
+        visited = new boolean[verticesCount];
+
+        for (int i = 0; i < t.length; i++) {
+            if (!isVertexVisited(t[i])) {
+                exploreVertex(t[i], t[i]);
+                parentCount++;
+            }
+        }
+        int[] sccSize = new int[verticesCount];
+
+        for (int parent: parents)
+            sccSize[parent - 1]++;
+
+        Arrays.sort(sccSize);
+        int[] result = new int[5];
+        for (int i = 0; i < 5; i++) {
+            result [i] = sccSize[sccSize.length - 1 -i];
+        }
+        return result;
+    }
+
+    private void exploreVertex(int i, int parent) {
+        if (isVertexVisited(i))
+            return;
+
+        visited[i - 1] = true;
+        List connected = workingGraph.get(i - 1);
+        for (Object vertex : connected) {
+            exploreVertex(Integer.valueOf(vertex.toString()), parent);
+        }
+
+        if (parent == 0) setT(i);
+        else setParent(i, parent);
+
+    }
+
+    private void setParent(int vertex, int parent) {
+        parents[vertex - 1] = parent;
+    }
+
+    private void setT(int i) {
+        t[--tCounter] = i;
+    }
+
+    private boolean isVertexVisited(int i) {
+        return visited[i - 1] == true;
+    }
+}
---
+++ coursera/algorithms_divide_conquer/Solutions/src/course2/week1/SCCFinderLauncher.java
@@ -0,0 +1,47 @@
+package course2.week1;
+
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+/**
+ * Created by b06514a on 4/27/2017.
+ */
+public class SCCFinderLauncher {
+
+    public static void main(String[] args) {
+
+        Path inputFile = null;
+        try {
+            inputFile = Paths.get(new SCCFinderLauncher().getClass().getResource("SCC.txt").toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        List input = null;
+        try {
+            input = Files.readAllLines(inputFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<List<Integer>> graphList = new ArrayList<>(857715);
+
+        for (Object row: input) {
+            String[] vertices = row.toString().split(" ");
+            int tail = Integer.valueOf(vertices[0]), head = Integer.valueOf(vertices[1]);
+            while (graphList.size() <= tail - 1)
+                graphList.add(new LinkedList<Integer>());
+            graphList.get(tail - 1).add(head);
+        }
+
+        System.out.println("Parsed file");
+
+        SCCFinder sccFinder = new SCCFinder(graphList);
+        for (int size: sccFinder.find())
+            System.out.println(size);
+    }
+
+}
